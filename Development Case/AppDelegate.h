df14a648d446709606b39705ab33104097dd7067
Simon Yang <simonyang81@gmail.com>
2016-11-16 18:29:41 +0800

    Initial Commit

---
+++ Development Case/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Development Case
+//
+//  Created by Simon Yang on 11/16/16.
+//  Copyright © 2016 Simon Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
